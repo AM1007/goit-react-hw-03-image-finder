120fe95f9054aa595c0c67974f354d75fbb9a836
AM1007 <andrew.motko@gmail.com>
2022-12-12 11:48:38 +0200

    loader added

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BallTriangle } from 'react-loader-spinner';
+import { InfinitySpin } from 'react-loader-spinner';
 import s from './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={s.wrapper}>
-      <BallTriangle
-        height="200"
-        width="200"
-        color="#3f51b5"
-        ariaLabel="loading"
-      />
+      <InfinitySpin width="200" color="#3f51b5" />
     </div>
   );
 };
