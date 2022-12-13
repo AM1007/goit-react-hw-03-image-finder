a5be66938c725284a23a918edb4fe67c2180738e
AM1007 <andrew.motko@gmail.com>
2022-12-14 01:25:28 +0200

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -10,6 +10,7 @@ axios.defaults.params = {
   image_type: 'photo',
   orientation: 'horizontal',
   per_page: IMAGES_PER_PAGE,
+  safe_search: true,
 };
 
 export const getImages = async (searchQuery, page) => {
@@ -22,3 +23,17 @@ export const getImages = async (searchQuery, page) => {
   const response = await axios.get('', config);
   return response.data;
 };
+
+// axios.defaults.baseURL = 'https://pixabay.com/api';
+// const API_KEY = '32013564-eb97bf6234748c3565a98072a';
+// const PARAM =
+//   'per_page=12&orientation=horizontal&image_type=photo&safesearch=true';
+
+// const getImages = async (searchQuery, page) => {
+//   const { data } = await axios.get(
+//     `/?key=${API_KEY}&q=${searchQuery}&page=${page}&${PARAM}`
+//   );
+//   return data;
+// };
+
+// export { getImages };
